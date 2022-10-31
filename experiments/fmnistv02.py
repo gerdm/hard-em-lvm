d95271e2cc0774c58e9cc4bc0ca61ff84646de91
Gerardo Duran-Martin <g.duran@me.com>
2022-10-31 12:46:52 +0000

    feat: fmnistv02
    Add convolutional VAE

--- experiments/fmnistv02.py
+++ experiments/fmnistv02.py
@@ -14,23 +14,22 @@ os.environ["TPU_CHIPS_PER_HOST_BOUNDS"] = "1,1,1"
 os.environ["TPU_HOST_BOUNDS"] = "1,1,1"
 os.environ["TPU_VISIBLE_DEVICES"] = "2"
 
-
 class Decoder(nn.Module):
     """
     Based on:
     https://github.com/probml/probml-utils/blob/main/probml_utils/conv_vae_flax_utils.py
     """
-    dim_obs: Tuple[int, int, int]
-    hidden_channels: Sequence[int]
+    dim_obs: Tuple[int, int, int] # (H)eight, (W)idth, number of (C)hannels
     dim_latent: int = 20
+    hidden_channels: Sequence[int] = (32, 64, 128, 256, 512)
 
     @nn.compact
-    def __call__(self, X, training):
-        H, W, C = self.output_dim
+    def __call__(self, X, training=True):
+        H, W, C = self.dim_obs
 
         # TODO: relax this restriction
         factor = 2 ** len(self.hidden_channels)
-        assert (
+        assert(
             H % factor == W % factor == 0
         ), f"output_dim must be a multiple of {factor}"
         H, W = H // factor, W // factor
@@ -43,7 +42,7 @@ class Decoder(nn.Module):
             X = nn.ConvTranspose(
                 hidden_channel, (3, 3), strides=(2, 2), padding=((1, 2), (1, 2))
             )(X)
-            X = nn.BatchNorm(use_running_average=not training)(X)
+            # X = nn.BatchNorm(use_running_average=not training)(X)
             X = nn.elu(X)
 
         X = nn.ConvTranspose(C, (3, 3), strides=(2, 2), padding=((1, 2), (1, 2)))(X)
@@ -52,13 +51,13 @@ class Decoder(nn.Module):
 
 class Encoder(nn.Module):
     latent_dim: int
-    hidden_channels: Sequence[int]
+    hidden_channels: Sequence[int] = (32, 64, 128, 256, 512)
 
     @nn.compact
-    def __call__(self, X, training):
+    def __call__(self, X, training=True):
         for channel in self.hidden_channels:
             X = nn.Conv(channel, (3, 3), strides=2, padding=1)(X)
-            X = nn.BatchNorm(use_running_average=not training)(X)
+            # X = nn.BatchNorm(use_running_average=not training)(X)
             X = nn.elu(X)
 
         X = X.reshape((-1, np.prod(X.shape[-3:])))
@@ -66,6 +65,53 @@ class Encoder(nn.Module):
         logvar = nn.Dense(self.latent_dim)(X)
 
         return mu, logvar
+
+
+class ConvEncoder(nn.Module):
+    latent_dim: tuple
+
+    @nn.compact
+    def __call__(self, x):
+        import ipdb; ipdb.set_trace()
+        z = nn.Conv(5, (3, 3), padding="SAME")(x)
+        z = nn.elu(z)
+        z = nn.max_pool(z, (2, 2), padding="SAME")
+        z = z.reshape((z.shape[0], -1))
+        z = nn.Dense(self.latent_dim)(z)
+
+        mean_z = nn.Dense(self.latent_dim)(z)
+        logvar_z = nn.Dense(self.latent_dim)(z)
+        return mean_z, logvar_z
+
+
+class ConvDecoder(nn.Module):
+    dim_obs: Tuple
+    dim_latent: int
+
+    @nn.compact
+    def __call__(self, z):
+        x = nn.Dense(28 ** 2)(z)
+        x = x.reshape(-1, *self.dim_obs)
+        x = nn.elu(x)
+        x = nn.Conv(5, (3, 3), padding="SAME")(x)
+        x = nn.elu(x)
+        x = nn.Conv(1, (3, 3), padding="SAME")(x)
+        return x
+
+
+class ConvVAE(nn.Module):
+    observed_dims: tuple
+    latent_dim: int
+
+    def setup(self):
+        self.encoder = ConvEncoder(self.latent_dim)
+        self.decoder = ConvDecoder(self.observed_dims)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def __call__(self, x):
+        return self.decoder(self.encoder(x))
 
 
 if __name__ == "__main__":
@@ -82,21 +128,23 @@ if __name__ == "__main__":
     warmup, test = hlax.datasets.load_fashion_mnist(num_warmup, num_test, melt=False)
     X_warmup, X_test = warmup[0], test[0]
 
-    dict_models = {
-        "class_decoder": Decoder,
-        "class_encoder": Encoder,   
-        "class_encoder_test": hlax.models.GaussEncoder,
-        "class_vae": hlax.models.VAEGauss,
-    }
+    # # Resizing to 32x32
+    # X_warmup = jax.vmap(jax.image.resize, (0, None, None))(X_warmup, (32, 32), "nearest")
+    # X_test = jax.vmap(jax.image.resize, (0, None, None))(X_test, (32, 32), "nearest")
+    # # Reshaping to 32x32x1
+    # X_warmup = X_warmup[..., None]
+    # X_test = X_test[..., None]
 
     key = jax.random.PRNGKey(314)
-    lossfn_vae = hlax.losses.iwae
+    lossfn_vae = hlax.losses.iwae_bern
     lossfn_hardem = hlax.losses.loss_hard_nmll
+
+    hidden_channels = (32, 64, 128, 256, 512)
 
     _, *dim_obs = X_warmup.shape
     dim_latent = config["setup"]["dim_latent"]
-    model_vae = hlax.models.VAEBern(dim_latent, dim_obs, Encoder, Decoder)
-    model_decoder = Decoder(dim_obs, dim_latent)
+    model_vae = hlax.models.VAEBern(dim_latent, dim_obs, ConvEncoder, ConvDecoder)
+    model_decoder = ConvDecoder(dim_obs, dim_latent)
     model_encoder_test = hlax.models.GaussEncoder(dim_latent)
 
     output = base_vae_hardem.main(
@@ -122,4 +170,3 @@ if __name__ == "__main__":
     print(f"Saving {now}")
     with open(f"./experiments/outputs/experiment-{now}.pkl", "wb") as f:
         pickle.dump(output, f)
-
