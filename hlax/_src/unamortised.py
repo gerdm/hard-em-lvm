137acb79188b7d92cba274ad50e1b609ead685fe
Gerardo Duran-Martin <g.duran@me.com>
2022-11-18 18:12:50 +0000

    refactor: unamortised.py
    Separate encoder-decoder optimisers

--- hlax/_src/unamortised.py
+++ hlax/_src/unamortised.py
@@ -3,7 +3,7 @@ Unamortised VAE
 """
 
 import jax
-import chex 
+import chex
 import hlax
 import optax
 import jax.numpy as jnp
@@ -19,50 +19,14 @@ from flax.training.train_state import TrainState
 
 @dataclass
 class Config:
-    batch_size: int
     num_epochs: int
-    num_is_samples: int
-    dim_latent: int
-    tx: optax.GradientTransformation
-    model: nn.Module
+    batch_size: int
     num_e_steps: int
-    num_m_steps: int = 0
 
 
 @dataclass
-class CheckpointsConfig:
-    model: nn.Module
-    num_epochs: int
-    batch_size: int
-    dim_latent: int
+class CheckpointsConfig(Config):
     eval_epochs: list
-
-    num_e_steps: int
-    num_m_steps: int
-
-    tx: optax.GradientTransformation
-    num_is_samples: int
-
-
-def load_test_config(
-    dict_config: Dict,
-    model: nn.Module,
-) -> Config:
-    learning_rate = dict_config["test"]["learning_rate"]
-    tx = optax.adam(learning_rate)
-
-    config = Config(
-        batch_size=dict_config["test"]["batch_size"],
-        num_epochs=dict_config["test"]["num_epochs"],
-        num_is_samples=dict_config["test"]["num_is_samples"],
-        dim_latent=dict_config["setup"]["dim_latent"],
-        num_e_steps=dict_config["test"]["num_e_steps"],
-        num_m_steps=dict_config["test"]["num_m_steps"],
-        tx=tx,
-        model=model,
-    )
-    return config
-
 
 def load_config(
     dict_config: Dict,
@@ -89,41 +53,23 @@ def load_config(
 
 
 @jax.jit
-def get_batch_adam_params_encoder(opt_state, ixs):
-    """
-    Get mu and nu optimiser parameters for a batch of
-    observations.
-    """
-    encoder_sub_mu = opt_state[0].mu["params"]["encoder"]
-    encoder_sub_nu = opt_state[0].nu["params"]["encoder"]
-    
-    encoder_sub_mu = jax.tree_map(lambda x: x[ixs], encoder_sub_mu)
-    encoder_sub_nu = jax.tree_map(lambda x: x[ixs], encoder_sub_nu)
-    
-    return encoder_sub_mu, encoder_sub_nu
-
-
-@jax.jit
 def create_batch_adam_params(opt_state, ixs):
-    mu_sub, nu_sub = get_batch_adam_params_encoder(opt_state, ixs)
-    
-    opt_state_batch = opt_state[0]
-    mu_params = unfreeze(opt_state_batch.mu)
-    nu_params = unfreeze(opt_state_batch.nu)
+    sub_mu = opt_state[0].mu
+    sub_nu = opt_state[0].nu
 
-    # Replace encoder opt params with indexed params
-    mu_params["params"]["encoder"] = mu_sub
-    nu_params["params"]["encoder"] = nu_sub
-    
+    sub_mu = jax.tree_map(lambda x: x[ixs], sub_mu)
+    sub_nu = jax.tree_map(lambda x: x[ixs], sub_nu)
+
+    opt_state_batch = opt_state[0]
     opt_state_batch = opt_state_batch._replace(
-        mu=freeze(mu_params),
-        nu=freeze(nu_params)
+        mu=sub_mu,
+        nu=sub_nu
     )
-    
+
     opt_state_batch = (
         opt_state_batch,
     ) + opt_state[1:]
-    
+
     return opt_state_batch
 
 
@@ -131,7 +77,7 @@ def create_batch_adam_params(opt_state, ixs):
 def update_pytree(pytree, pytree_subset, ixs):
     """
     Update the subset of a pytree.
-    
+
     Parameters
     ----------
     pytree:
@@ -150,31 +96,24 @@ def update_pytree(pytree, pytree_subset, ixs):
 
 
 @jax.jit
-def create_state_batch(state, ixs):
+def create_state_encoder_batch(state_encoder, ixs):
     """
     Create a TrainState object for a batch of observations.
     We create this train state with
     1. The subset of parameters for the batch
     2. The subset of optimiser parameters for the batch
     """
-    params_batch_encoder = jax.tree_map(lambda x: x[ixs], state.params["params"]["encoder"])
-    params_batch = freeze({
-        "params": {
-            "encoder": params_batch_encoder,
-            "decoder": state.params["params"]["decoder"]
-        }
-    })
-    
-    opt_state_batch = create_batch_adam_params(state.opt_state, ixs)
-    
+    params_batch_encoder = jax.tree_map(lambda x: x[ixs], state_encoder.params)
+    opt_state_batch = create_batch_adam_params(state_encoder.opt_state, ixs)
+
     state_batch = TrainState(
-        step=state.step,
-        apply_fn=state.apply_fn,
-        tx=state.tx,
-        params=params_batch,
+        step=state_encoder.step,
+        apply_fn=state_encoder.apply_fn,
+        tx=state_encoder.tx,
+        params=params_batch_encoder,
         opt_state=opt_state_batch,
     )
-    
+
     return state_batch
 
 
@@ -206,63 +145,46 @@ def adam_replace_opt_state(state, old_mu, old_nu, entry_name):
     return state_update
 
 
-def e_step(_, state, lossfn, X, zero_grads_m, old_mu_params, old_nu_params):
-    params_encoder = state.params["params"]["encoder"]
-    params_decoder = state.params["params"]["decoder"]
+@partial(jax.jit, static_argnames=("lossfn",))
+def e_step(
+    _: int,
+    state_encoder: TrainState,
+    state_decoder: TrainState,
+    lossfn: Callable,
+    X: chex.ArrayDevice
+):
+    params_encoder = state_encoder.params
+    params_decoder = state_decoder.params
     grads_encoder = jax.grad(lossfn, 0)(params_encoder, params_decoder, X)
-    grads_patch = freeze({
-        "params": {
-            "encoder": grads_encoder,
-            "decoder": zero_grads_m
-        }
-    })
-    state = state.apply_gradients(grads=grads_patch)
-    state = adam_replace_opt_state(
-        state,
-        old_mu_params,
-        old_nu_params,
-        "decoder"
-    )
-    return state
+
+    state_encoder = state_encoder.apply_gradients(grads=grads_encoder)
+    return state_encoder
 
 
 @jax.jit
-def m_step(state, grads_decoder, num_batches):
+def m_step(state_decoder, grads_decoder, num_batches):
     """
     Update the decoder parameters after accumulating
     gradients over a number of batches in the E-step.
     """
-    params_fixed = "encoder"
     grads_decoder = jax.tree_map(lambda x: x / num_batches, grads_decoder)
-    old_mu_params = state.opt_state[0].mu["params"][params_fixed]
-    old_nu_params = state.opt_state[0].nu["params"][params_fixed]
-
-    params_encoder = state.params["params"]["encoder"]
-    grads_zero_encoder = jax.tree_map(lambda _: 0, params_encoder)
-    grads_patch = freeze({
-        "params": {
-            "encoder": grads_zero_encoder,
-            "decoder": grads_decoder
-        }
-    })
-    state = state.apply_gradients(grads=grads_patch)
-
-    state = adam_replace_opt_state(
-        state,
-        old_mu_params,
-        old_nu_params,
-        "encoder"
-    )
-    return state
+    state_decoder = state_decoder.apply_gradients(grads=grads_decoder)
+    return state_decoder
 
 
 @partial(jax.jit, static_argnames=("lossfn",))
-def update_state_batch_em(key, X_batch, state_batch,
-                          num_e_steps, _, lossfn):
+def update_state_batch_em(
+    key: chex.ArrayDevice,
+    X_batch: chex.ArrayDevice,
+    state_batch_encoder: TrainState,
+    state_decoder: TrainState,
+    num_e_steps: int,
+    lossfn: Callable,
+):
     """
     Update the train state using the EM algorithm.
     """
-    apply_fn = state_batch.apply_fn
+    apply_fn = state_batch_encoder.apply_fn
     def part_lossfn(params_encoder, params_decoder, X):
         params = freeze({
             "params": {
@@ -273,46 +195,20 @@ def update_state_batch_em(key, X_batch, state_batch,
         return lossfn(key, params, apply_fn, X)
 
     # E-step
-    params_fixed = "decoder"
-    params_decoder = state_batch.params["params"][params_fixed]
-    grads_zero_decoder = jax.tree_map(lambda _: 0, params_decoder)
-    old_mu_params = state_batch.opt_state[0].mu["params"][params_fixed]
-    old_nu_params = state_batch.opt_state[0].nu["params"][params_fixed]
-
     part_e_step = partial(
         e_step,
         lossfn=part_lossfn,
         X=X_batch,
-        zero_grads_m=grads_zero_decoder,
-        old_mu_params=old_mu_params,
-        old_nu_params=old_nu_params
+        state_decoder=state_decoder,
     )
-    state_batch = jax.lax.fori_loop(0, num_e_steps, part_e_step, state_batch)
+    state_batch_encoder = jax.lax.fori_loop(0, num_e_steps, part_e_step, state_batch_encoder)
 
     # M-step preprocessing: Accumulate gradients
-    params_encoder = state_batch.params["params"]["encoder"]
-    params_decoder = state_batch.params["params"]["decoder"]
+    params_encoder = state_batch_encoder.params
+    params_decoder = state_decoder.params
     loss_batch, grads_decoder = jax.value_and_grad(part_lossfn, 1)(params_encoder, params_decoder, X_batch)
 
-    return state_batch, grads_decoder, loss_batch
-
-
-@jax.jit
-def reconstruct_params(state, new_state_batch, ixs):
-    params_encoder_update = unfreeze(state.params["params"]["encoder"])
-    params_batch_encoder_update = unfreeze(new_state_batch.params["params"]["encoder"])
-    params_encoder_update = update_pytree(params_encoder_update, params_batch_encoder_update, ixs)
-
-    params_decoder_update = unfreeze(new_state_batch.params["params"]["decoder"])
-
-    params_update = freeze({
-        "params": {
-            "encoder": params_encoder_update,
-            "decoder": params_decoder_update,
-        }
-    })
-
-    return params_update
+    return state_batch_encoder, grads_decoder, loss_batch
 
 
 @jax.jit
@@ -320,25 +216,17 @@ def reconstruct_opt_state(state, new_state_batch, ixs):
     mu_state = unfreeze(state.opt_state[0].mu)
     nu_state = unfreeze(state.opt_state[0].nu)
 
-
-    # Update decoder optimisation-state params
-    mu_state["params"]["decoder"] = new_state_batch.opt_state[0].mu["params"]["decoder"]
-    nu_state["params"]["decoder"] = new_state_batch.opt_state[0].nu["params"]["decoder"]
-
     # Update encoder optimisation-state params
-    batch_mu_encoder = unfreeze(new_state_batch.opt_state[0].mu["params"]["encoder"])
-    mu_state["params"]["encoder"] = update_pytree(mu_state["params"]["encoder"], batch_mu_encoder, ixs)
+    batch_mu_encoder = unfreeze(new_state_batch.opt_state[0].mu)
+    mu_state = update_pytree(mu_state, batch_mu_encoder, ixs)
 
-    batch_nu_encoder = unfreeze(new_state_batch.opt_state[0].nu["params"]["encoder"])
-    nu_state["params"]["encoder"] = update_pytree(nu_state["params"]["encoder"], batch_nu_encoder, ixs)
-
-    mu_state = freeze(mu_state)
-    nu_state = freeze(nu_state)
+    batch_nu_encoder = unfreeze(new_state_batch.opt_state[0].nu)
+    nu_state = update_pytree(nu_state, batch_nu_encoder, ixs)
 
     opt_state_update = (
         state.opt_state[0]._replace(
-            mu=mu_state,
-            nu=nu_state,
+            mu=freeze(mu_state),
+            nu=freeze(nu_state),
         ),
     ) + state.opt_state[1:]
 
@@ -358,17 +246,25 @@ def update_reconstruct_state(state, new_params, new_opt_state):
 
 
 @partial(jax.jit, static_argnames=("lossfn",))
-def train_step_batch(key, X, state, ixs, num_e_steps, num_m_steps, lossfn):
+def train_step_batch(
+    key: chex.ArrayDevice,
+    X: chex.ArrayDevice,
+    state_encoder: TrainState,
+    state_decoder: TrainState,
+    ixs: chex.ArrayDevice,
+    num_e_steps: int,
+    lossfn: Callable,
+):
     X_batch = X[ixs]
-    state_batch = create_state_batch(state, ixs)
-    new_state_batch, m_step_grads, loss_batch = update_state_batch_em(
-        key, X_batch, state_batch, num_e_steps, num_m_steps, lossfn
+    state_encoder_batch = create_state_encoder_batch(state_encoder, ixs)
+    state_encoder_batch, m_step_grads, loss_batch = update_state_batch_em(
+        key, X_batch, state_encoder_batch, state_decoder, num_e_steps, lossfn
     )
-    
-    new_params = reconstruct_params(state, new_state_batch, ixs)
-    new_opt_state = reconstruct_opt_state(state, new_state_batch, ixs)
-    new_state = update_reconstruct_state(state, new_params, new_opt_state)
-    return new_state, m_step_grads, loss_batch
+
+    new_encoder_params = update_pytree(state_encoder.params, state_encoder_batch.params, ixs)
+    new_encoder_opt_state = reconstruct_opt_state(state_encoder, state_encoder_batch, ixs)
+    new_state_encoder = update_reconstruct_state(state_encoder, new_encoder_params, new_encoder_opt_state)
+    return new_state_encoder, m_step_grads, loss_batch
 
 
 @jax.jit
@@ -377,7 +273,16 @@ def accumulate_grads(all_grads, grads):
     return all_grads
 
 
-def train_epoch(key, X, state, batch_size, num_e_steps, num_m_steps, lossfn):
+def train_epoch(
+    key: jnp.ndarray,
+    X: jnp.ndarray,
+    state_encoder: TrainState,
+    state_decoder: TrainState,
+    batch_size: int,
+    num_e_steps: int,
+    num_m_steps: int,
+    lossfn: Callable,
+):
     num_obs = X.shape[0]
     key_batch, key_train = jax.random.split(key)
     batch_ixs = hlax.training.get_batch_train_ixs(key_batch, num_obs, batch_size)
@@ -385,23 +290,26 @@ def train_epoch(key, X, state, batch_size, num_e_steps, num_m_steps, lossfn):
     keys_train = jax.random.split(key_train, num_batches)
 
     total_loss = 0
-    m_grads = jax.tree_map(lambda x: jnp.zeros_like(x), state.params["params"]["decoder"])
+    m_grads = jax.tree_map(lambda x: jnp.zeros_like(x), state_decoder.params)
     for batch_ix, key_epoch in zip(batch_ixs, keys_train):
-        state, m_step_grads, loss_batch = train_step_batch(
-            key_epoch, X, state, batch_ix, num_e_steps, num_m_steps, lossfn
+        state_encoder, m_step_grads, loss_batch = train_step_batch(
+            key_epoch, X, state_encoder, state_decoder, batch_ix, num_e_steps, lossfn
         )
         total_loss += loss_batch
         if num_m_steps > 0:
             m_grads = accumulate_grads(m_grads, m_step_grads)
-    state = m_step(state, m_grads, num_batches)
-    return total_loss, state
+    state_decoder = m_step(state_decoder, m_grads, num_batches)
+    return total_loss, state_encoder, state_decoder
 
 
 def train_checkpoints(
     key: chex.ArrayDevice,
+    model: nn.Module,
     config: CheckpointsConfig,
     X: chex.ArrayDevice,
     lossfn: Callable,
+    tx_encoder: optax.GradientTransformation,
+    tx_decoder: optax.GradientTransformation,
 ):
     """
     Find inference model parameters theta at multiple
@@ -416,84 +324,46 @@ def train_checkpoints(
     keys_train = jax.random.split(key_train, config.num_epochs)
 
     batch_init = jnp.ones((num_obs, *dim_obs))
-    params_init = config.model.init(key_params_init, batch_init, key_eps_init, num_samples=3)
+    params_init = model.init(key_params_init, batch_init, key_eps_init, num_samples=3)
 
-    state = TrainState.create(
-        apply_fn=partial(config.model.apply, num_samples=config.num_is_samples),
-        params=params_init,
-        tx=config.tx,
+    params_encoder_init = params_init["params"]["encoder"]
+    params_decoder_init = params_init["params"]["decoder"]
+
+    state_encoder = TrainState.create(
+        apply_fn=model.apply,
+        params=params_encoder_init,
+        tx=tx_encoder,
+    )
+
+    state_decoder = TrainState.create(
+        apply_fn=model.apply,
+        params=params_decoder_init,
+        tx=tx_decoder,
     )
 
     num_e_steps = config.num_e_steps
-    num_m_steps = config.num_m_steps
+    num_m_steps = 1
     time_init = time()
     for e, keyt in (pbar := tqdm(enumerate(keys_train), total=config.num_epochs)):
-        loss, state = train_epoch(keyt, X, state, config.batch_size,
-                                  num_e_steps, num_m_steps, lossfn)
+        loss, state_encoder, state_decoder = train_epoch(
+            keyt, X, state_encoder, state_decoder, config.batch_size,
+            num_e_steps, num_m_steps, lossfn
+        )
 
         hist_loss.append(loss)
         pbar.set_description(f"loss={loss:0.5e}")
 
         if (enum := e + 1) in config.eval_epochs:
             time_ellapsed = time() - time_init
-            params_decoder = freeze({"params": unfreeze(state.params)["params"]["decoder"]})
+            params_decoder = state_decoder.params
 
             dict_times[f"e{enum}"] = time_ellapsed
             dict_params[f"e{enum}"] = params_decoder
-    
+
     output = {
         "times": dict_times,
         "checkpoint_params": dict_params,
         "hist_loss": jnp.array(hist_loss),
-        "state_final": state
+        "state_final": (state_encoder, state_decoder)
     }
     return output
-
-
-def test_decoder(
-    key: chex.ArrayDevice,
-    config: CheckpointsConfig,
-    X: chex.ArrayDevice,
-    lossfn: Callable,
-    decoder_params: nn.FrozenDict,
-):
-    """
-    Test the decoder parameters by learning the parameters
-    of an unamortised encoder using the IWAE estimator.
-    For this, we keep the parameter of the decoder fixed
-    (num_m_steps=0) and we train the encoder for config.num_epochs
-    epochs.
-    """
-    hist_loss = []
-    num_obs, *dim_obs = X.shape
-    key_params_init, key_eps_init, key_train = jax.random.split(key, 3)
-    keys_train = jax.random.split(key_train, config.num_epochs)
-    batch_init = jnp.ones((num_obs, *dim_obs))
-    params_init = config.model.init(key_params_init, batch_init, key_eps_init)
-
-    # Replace the parameters of the decoder with the
-    # learned parameters
-    params_init = unfreeze(params_init)
-    params_init["params"]["decoder"] = decoder_params
-    params_init = freeze(params_init)
-
-    state = TrainState.create(
-        apply_fn=partial(config.model.apply, num_samples=config.num_is_samples),
-        params=params_init,
-        tx=config.tx,
-    )
-
-    num_e_steps = config.num_e_steps
-    num_m_steps = config.num_m_steps
-    pbar = tqdm(keys_train, leave=False)
-    for keyt in pbar:
-        loss, state = train_epoch(keyt, X, state, config.batch_size,
-                                  num_e_steps, num_m_steps, lossfn)
-        pbar.set_description(f"loss={loss:0.5e}")
-        hist_loss.append(loss)
-    
-    res = {
-        "hist_loss": jnp.array(hist_loss),
-        "state": state
-    }
-    return res
